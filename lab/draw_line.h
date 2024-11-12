c260716fd14a60545bd07563d425cf6a17db5344
ZhukovaEkaterinaB21-215 <116277216+ZhukovaEkaterinaB21-215@users.noreply.github.com>
2024-11-12 17:26:49 +0300

    Delete lab directory

--- lab/draw_line.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void drawLine(Mat& image, Point start, Point end, string color);
--- lab/draw_polygon.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void drawPolygon(Mat& image, vector<Point> vertex, string color);
-vector<Point> calculateStarVertices(Mat& image, double radiusf, double radiuss, int number);
